f0eaf103285ce2e76ff508c6239b9a5a5ba1ae79
JeongB-L <70557130+JeongB-L@users.noreply.github.com>
2020-12-19 15:48:09 +0900

    Update app.py

--- MaskMandator/src/app.py
+++ MaskMandator/src/app.py
@@ -14,24 +14,6 @@ app.config['UPLOAD_FOLDER'] = './'
 def home():
    return render_template('index.html')
 
-#@app.route('/upload', methods=['GET', 'POST'])
-#def upload_file(file):
-#    if request.method == 'POST':
-#        # check if the post request has the file part
-#        if 'file' not in request.files:
-#            flash('No file part')
-#            return redirect(request.url)
-#        file = request.files['file']
-#        # if user does not select file, browser also
-#        # submit an empty part without filename
-#        if file.filename == '':
-#            flash('No selected file')
-#            return redirect(request.url)
-#        if file and allowed_file(file.filename):
-#            filename = file.filename
-#            file.save(os.path.join(app.config['uploaded.png'], filename))
-#            context = {'return_string', processUserInput.main()}
-#            return render_template('index.html', context=context)
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
